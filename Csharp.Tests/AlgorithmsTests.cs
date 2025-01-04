ecff3c54e5ed091d9f654a5b895338b844030dc2
Manoj Baishya <28330014+manojbaishya@users.noreply.github.com>
2025-01-04 08:43:00 +0530

    Add FFT()

--- Csharp.Tests/AlgorithmsTests.cs
+++ Csharp.Tests/AlgorithmsTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Xunit.Abstractions;
 using FluentAssertions;
+using System.Numerics;
 
 namespace Csharp.Tests;
 
@@ -40,5 +41,36 @@ public class RuntimeAnalysisTests(ITestOutputHelper logger)
         IList<int> listDoesNotHaveDuplicates = [1, 2, 3, 4];
         RuntimeAnalysis.HasDuplicates(listDoesNotHaveDuplicates).Should().BeFalse("because has no duplicates.");
         logger.WriteLine($"{nameof(listHasDuplicates)} returned false!");
+    }
+}
+
+
+public class PolynomialTests(ITestOutputHelper logger)
+{
+
+    private readonly ITestOutputHelper logger = logger;
+
+    [Fact]
+    public void TestPolynomialToString()
+    {
+        Polynomial polynomial = new(1, 2, 3, 4, 5);
+        var expectedPolynomial = "1 + 2x + 3x^2 + 4x^3 + 5x^4";
+        var actualPolynomial = polynomial.ToString();
+
+        logger.WriteLine($"Actual Polynomial is: '{actualPolynomial}'");
+
+        actualPolynomial.Should().Be(expectedPolynomial, "because actualPolynomial is equal to expectedPolynomial.");
+    }
+
+    [Fact]
+    public void TestPolynomialComputeFft() {
+        Polynomial polynomial = new(6, 5, 8, 32);
+
+        Complex[] actualFft = [new Complex(51, 0), new Complex(-2, -27), new Complex(-23, 0), new Complex(-2, 27)];
+        Complex[] expectedFft = polynomial.ComputeFft();
+
+        logger.WriteLine($"Actual Polynomial is: '{actualFft.Select(x => x.ToString()).Aggregate((x, y) => $"{x}, {y}")}'");
+
+        logger.WriteLine($"Expected Polynomial is: '{expectedFft.Select(x => x.ToString()).Aggregate((x, y) => $"{x}, {y}")}'");
     }
 }
--- Csharp/Algorithms.cs
+++ Csharp/Algorithms.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 
 namespace Csharp;
 
@@ -55,6 +56,42 @@ public class RuntimeAnalysis
     }
 }
 
-public class Polynomials {
+public class Polynomial(params double[] coefficients)
+{
+    public double[] Coefficients { get; } = coefficients;
+    public Complex[]? Fft {get; private set; }
+    public int DegreeBound => Coefficients.Length;
 
+    public Complex[] ComputeFft() {
+        Fft = ComputeFft(Coefficients, DegreeBound);
+        return Fft;
+    }
+
+    private static Complex[] ComputeFft(double[] coefficients, int degreeBound) {
+        if (degreeBound == 1) return coefficients.Select(x => new Complex(x, 0)).ToArray();
+        var wn = Complex.Exp(2 * Math.PI * Complex.ImaginaryOne / degreeBound);
+        var w = new Complex(1, 0);
+
+        Complex[] fftEven = ComputeFft(coefficients.Where((_, i) => i % 2 == 0).ToArray(), degreeBound / 2);
+        Complex[] fftOdd = ComputeFft(coefficients.Where((_, i) => i % 2 == 1).ToArray(), degreeBound / 2);
+
+        Complex[] fft = new Complex[degreeBound];
+        for (int i = 0; i < degreeBound / 2; i++) {
+            fft[i] = fftEven[i] + w * fftOdd[i];
+            fft[i + degreeBound / 2] = fftEven[i] - w * fftOdd[i];
+            w *= wn;
+        }
+
+        return fft;
+    }
+
+    public override string ToString() {
+        string polynomial = "";
+        for (int i = 0; i < Coefficients.Length; i++) {
+            if (i == 0) polynomial += $"{Coefficients[i]}";
+            else if (i == 1) polynomial += $" + {Coefficients[i]}x";
+            else polynomial += $" + {Coefficients[i]}x^{i}";
+        }
+        return polynomial;
+    }
 }
