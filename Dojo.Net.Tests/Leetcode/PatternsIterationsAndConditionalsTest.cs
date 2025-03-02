c32e709566f6fcc7de9873b66eb9e12e60ecf081
Manoj Baishya <manojbaishya@users.noreply.github.com>
2025-03-02 10:11:59 +0530

    Added patterns for conceptual understanding of algorithm execution work

---
+++ Dojo.Net.Tests/Leetcode/PatternsIterationsAndConditionalsTest.cs
@@ -0,0 +1,60 @@
+﻿using Dojo.Net.Leetcode;
+using JetBrains.Annotations;
+using Xunit;
+
+namespace Dojo.Net.Tests.Leetcode;
+
+[TestSubject(typeof(PatternsIterationsAndConditionals))]
+public class PatternsIterationsAndConditionalsTest
+{
+
+    [Fact]
+    public void NDimensionalForestBruteForce()
+    {
+        int n = 10;
+        var sut = new PatternsIterationsAndConditionals();
+        sut.NDimensionalForestBruteForce(n);
+    }
+    
+    [Fact]
+    public void NDimensionalForestCacheArray()
+    {
+        int n = 10;
+        var sut = new PatternsIterationsAndConditionals();
+        sut.NDimensionalForestCacheArray(n);
+    }
+
+    [Fact]
+    public void NBy2DimensionalForest()
+    {
+        int n = 10;
+        var sut = new PatternsIterationsAndConditionals();
+        sut.NBy2DimensionalForest(n);
+    }
+
+    [Fact]
+    public void NTriangle()
+    {
+        int n = 10;
+        var sut = new PatternsIterationsAndConditionals();
+        sut.NTriangle(n);
+    }
+    
+    [Fact]
+    public void NTriangleCached()
+    {
+        int n = 9;
+        var sut = new PatternsIterationsAndConditionals();
+        sut.NTriangleCached(n);
+    }
+    
+    [Fact]
+    public void NTriangleRows()
+    {
+        int n = 9;
+        var sut = new PatternsIterationsAndConditionals();
+        sut.NTriangleRows(n);
+    }
+
+
+}
---
+++ Dojo.Net/Leetcode/PatternsIterationsAndConditionals.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Dojo.Net.Leetcode;
+
+public class PatternsIterationsAndConditionals
+{
+    public void NDimensionalForestBruteForce(int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+    
+    public void NDimensionalForestCacheArray(int n)
+    {
+        char[] line = new char[n];
+        for (int j = 0; j < n; j++)
+        {
+            line[j] = '*';
+        }
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine(line);
+        }
+    }
+
+    public void NBy2DimensionalForest(int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public void NTriangle(int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                int compute = j + 1; // NxN compute steps
+                Console.Write(compute);
+            }
+            Console.WriteLine();
+        }
+    }
+    
+    public void NTriangleCached(int n)
+    {
+        int[] cache = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            int compute = i + 1; // Compute moved to outer loop
+            cache[i] = compute; // Cache results and reuse them
+            for (int j = 0; j <= i; j++)
+            {
+                Console.Write(cache[j]);
+            }
+            Console.WriteLine();
+        }
+    }
+    
+    public void NTriangleRows(int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                Console.Write(i + 1);
+            }
+            Console.WriteLine();
+        }
+    }
+}
