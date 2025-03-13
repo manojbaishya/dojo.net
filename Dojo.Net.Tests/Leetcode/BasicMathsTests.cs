0c5f678841afc8b821532d60eba9177d1b1097ae
Manoj Baishya <manojbaishya@users.noreply.github.com>
2025-03-13 15:17:09 +0530

    Added Basic Maths

--- Dojo.Net.Tests/Leetcode/BasicMathsTests.cs
+++ Dojo.Net.Tests/Leetcode/BasicMathsTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Dojo.Net.Leetcode;
 
 using JetBrains.Annotations;
@@ -14,10 +15,131 @@ public class BasicMathsTests(ITestOutputHelper logger)
 
     [Theory]
     [InlineData(12, 2)]
-    public void TestEvenlyDivides(int number, int expected)
+    public void EvenlyDivides(int number, int expected)
     {
         var sut = new BasicMaths();
         int actual = sut.EvenlyDivides(number);
+
+        _logger.WriteLine($"Expected: {expected}");
+        _logger.WriteLine($"Actual: {actual}");
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(9854, 4589)]
+    [InlineData(1534236469, 0)]
+    [InlineData(-9854, -4589)]
+    public void ReverseInteger(int number, int expected)
+    {
+        var sut = new BasicMaths();
+        int actual = sut.ReverseInteger(number);
+
+        _logger.WriteLine($"Expected: {expected}");
+        _logger.WriteLine($"Actual: {actual}");
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(121, true)]
+    [InlineData(-121, false)]
+    [InlineData(10, false)]
+    public void IsPalindrome(int number, bool expected)
+    {
+        var sut = new BasicMaths();
+        bool actual = sut.IsPalindrome(number);
+
+        _logger.WriteLine($"Expected: {expected}");
+        _logger.WriteLine($"Actual: {actual}");
+
+        Assert.Equal(expected, actual);
+    }
+
+
+    [Theory]
+    [InlineData(371, true)]
+    [InlineData(1634, false)]
+    [InlineData(35, false)]
+    public void IsArmstrongNumber(int number, bool expected)
+    {
+        var sut = new BasicMaths();
+        bool actual = sut.IsArmstrongNumber(number);
+
+        _logger.WriteLine($"Expected: {expected}");
+        _logger.WriteLine($"Actual: {actual}");
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(36, new int[] {36, 18, 12, 9, 6, 4, 3, 2, 1})]
+    public void GetAllDivisors(int number, int[] expected)
+    {
+        var sut = new BasicMaths();
+        int[] actual = [.. sut.GetAllDivisorsDesc(number)];
+
+        _logger.WriteLine($"Expected: {expected}");
+        _logger.WriteLine($"Actual: {actual}");
+
+        Assert.Equivalent(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(36, false)]
+    [InlineData(13, true)]
+    [InlineData(11, true)]
+    [InlineData(1, false)]
+    public void IsPrime(int number, bool expected)
+    {
+        var sut = new BasicMaths();
+        bool actual = sut.IsPrime(number);
+
+        _logger.WriteLine($"Expected: {expected}");
+        _logger.WriteLine($"Actual: {actual}");
+
+        Assert.Equal(expected, actual);
+    }
+
+
+    [Theory]
+    [InlineData(12, 6, 6)]
+    [InlineData(27, 18, 9)]
+    [InlineData(11, 13, 1)]
+    public void HighestCommonFactor(int M, int N, int expected)
+    {
+        var sut = new BasicMaths();
+        int actual = sut.HighestCommonFactor(M, N);
+
+        _logger.WriteLine($"Expected: {expected}");
+        _logger.WriteLine($"Actual: {actual}");
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(12, 6, 6)]
+    [InlineData(27, 18, 9)]
+    [InlineData(11, 13, 1)]
+    public void HighestCommonFactorEuclidean(int M, int N, int expected)
+    {
+        var sut = new BasicMaths();
+        int actual = sut.HighestCommonFactorEuclidean(M, N);
+
+        _logger.WriteLine($"Expected: {expected}");
+        _logger.WriteLine($"Actual: {actual}");
+
+        Assert.Equal(expected, actual);
+    }
+
+        [Theory]
+    [InlineData(12, 6, 12)]
+    [InlineData(27, 18, 54)]
+    [InlineData(11, 13, 143)]
+    public void LeastCommonMultiple(int M, int N, int expected)
+    {
+        var sut = new BasicMaths();
+        int actual = sut.LeastCommonMultiple(M, N);
 
         _logger.WriteLine($"Expected: {expected}");
         _logger.WriteLine($"Actual: {actual}");
--- Dojo.Net/Leetcode/BasicMaths.cs
+++ Dojo.Net/Leetcode/BasicMaths.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Dojo.Net.Leetcode;
 
@@ -7,19 +9,109 @@ public class BasicMaths
     public int EvenlyDivides(int n) 
     {
         int num = n;
-
-        List<int> divDigits = new List<int>();
-
+        int count = 0;
         while (num > 0)
         {
             int lastDigit = num % 10;
             if (lastDigit != 0 && n % lastDigit == 0)
             {
-                divDigits.Add(lastDigit);
+                count++;
             }
             num /= 10;
         }
+        return count;
+    }
+
+    public int ReverseInteger(int x) 
+    {
+        int reverseNumber = 0;
+        int num = x;
+        int lastDigit;
+        try
+        {
+            while(num != 0)
+            {
+                lastDigit = num % 10;
+                reverseNumber = checked(reverseNumber * 10 + lastDigit);
+                num /= 10;
+            }
+        }
+        catch(OverflowException)
+        {
+            return 0;
+        }
         
-        return divDigits.Count;
+        return reverseNumber;
+    }
+
+    public bool IsPalindrome(int x) => x >= 0 && ReverseInteger(x) == x;
+
+    public bool IsArmstrongNumber(int num)
+    {
+        int copy = num;
+        int sum = 0;
+        while (copy != 0)
+        {
+            int lastDigit = copy % 10;
+            sum += lastDigit * lastDigit * lastDigit;
+            copy /= 10;
+        }
+        
+        return sum == num;
+    }
+
+    public List<int> GetAllDivisorsDesc(int N)
+    {
+        List<int> divisors = [];
+        var upperBound = Math.Sqrt(N);
+        for (int i = 1; i <= upperBound; i++)
+        {
+            if (N % i == 0)
+            {
+                divisors.Add(i);
+                int complement = N / i;
+                if (complement != i) divisors.Add(complement);
+            }
+        }
+
+        divisors.Sort((x, y) => y.CompareTo(x));
+        return divisors;
+    }
+
+    public bool IsPrime(int N)
+    {
+        var factors = GetAllDivisorsDesc(N);
+        if (factors.Count == 2 && factors.Contains(1) && factors.Contains(N)) return true;
+        else return false;
+    }
+
+    public int HighestCommonFactor(int M, int N)
+    {
+        var factorsM = GetAllDivisorsDesc(M);
+        var factorsN = GetAllDivisorsDesc(N);
+
+        int[] union = [.. factorsM.Intersect(factorsN)];
+        Array.Sort(union, (x, y) => y.CompareTo(x));
+
+        return union[0];
+    }
+
+    public int HighestCommonFactorEuclidean(int M, int N)
+    {
+        while(M > 0 && N > 0)
+        {
+            if (M > N) M %= N;
+            else N %= M;
+        }
+
+        if (M == 0) return N;
+        else return M;
+    }
+
+    public int LeastCommonMultiple(int M, int N)
+    {
+        int HCF = HighestCommonFactorEuclidean(M, N);
+        int LCM = (M * N) / HCF;
+        return LCM;
     }
 }
