ed375817faf25932a1b47bad502453111451e8bd
Manoj Baishya <manojbaishya@users.noreply.github.com>
2025-03-26 12:00:23 +0530

    Finished "76. Minimum Window Substring" with runtime: 27 ms and memory: 45.62 MB

--- Dojo.Net/Leetcode/SlidingWindow.cs
+++ Dojo.Net/Leetcode/SlidingWindow.cs
@@ -41,45 +41,39 @@ public class SlidingWindow
 
     public string MinWindow(string s, string t)
     {
-        if (s.Length < t.Length) return "";
+        if (s.Length < t.Length) return string.Empty;
         if (s.Equals(t)) return s;
 
-        Dictionary<char, int> cmpFreq = GetCharCounts(t.ToCharArray());
-
-        Dictionary<char, int> testFreq;
-        if (s.Length == t.Length)
-        {
-            testFreq = GetCharCounts(s.ToCharArray());
-            return CheckIfAllCharsExist(cmpFreq, testFreq) ? s : "";
-        }
-
+        Dictionary<char, int> cmpFreq = GetCharCounts(t);
+        Dictionary<char, int> testFreq = new(UniqueCharCount(s));
         PriorityQueue<Tuple<int, int>, int> minLengthsToWindows = new();
-        int st = 0;
-        Span<char> master = s.ToCharArray().AsSpan();
-        for (int en = 0; en < master.Length; en++)
+
+        int start = 0;
+        int missing = cmpFreq.Count;
+        for (int end = 0; end < s.Length; end++)
         {
-            if (en + 1 - st < t.Length) continue;
+            char inserted = s[end];
+            if (testFreq.TryGetValue(inserted, out int _)) testFreq[inserted]++;
+            else testFreq[inserted] = 1;
+            if (cmpFreq.TryGetValue(inserted, out int value) && testFreq.TryGetValue(inserted, out int cmpVal) && cmpVal == value) missing--;
 
-            ReadOnlySpan<char> window = master[st..(en + 1)];
-            // FIXME: Do not generate a new Dictionary for each substring window.
-            // TODO: Instead, have a master dictionary and update the counts as the iteration proceeds.
-            // TODO: Merge with L#52
-            testFreq = GetCharCounts(window);
+            if (end + 1 - start < t.Length) continue;
+            if (missing > 0) continue;
             
-            
-            if (!CheckIfAllCharsExist(cmpFreq, testFreq)) continue;
 
-            while (en + 1 - st >= t.Length)
+            while (missing == 0)
             {
-                char yanked = s[st];
+                char yanked = s[start];
+                
                 if (testFreq[yanked] > 1) testFreq[yanked]--;
                 else testFreq.Remove(yanked);
-
-                st++;
-                if (cmpFreq.TryGetValue(yanked, out int value) && (!testFreq.ContainsKey(yanked) || testFreq[yanked] < value)) break;
+                
+                if (cmpFreq.TryGetValue(yanked, out value) && !(testFreq.TryGetValue(yanked, out int cmpValue) && cmpValue >= value)) missing++;
+                
+                start++;
             }
 
-            minLengthsToWindows.Enqueue(new Tuple<int, int>(st - 1, en + 1), en + 1 - (st - 1));
+            minLengthsToWindows.Enqueue(new Tuple<int, int>(start - 1, end + 1), end + 1 - (start - 1));
         }
 
         if (minLengthsToWindows.Count == 0) return string.Empty;
@@ -90,7 +84,8 @@ public class SlidingWindow
 
     public static Dictionary<char, int> GetCharCounts(ReadOnlySpan<char> input)
     {
-        Dictionary<char, int> charCounts = new(input.Length);
+        int count = UniqueCharCount(input);
+        Dictionary<char, int> charCounts = new(count);
 
         foreach (char c in input)
         {
@@ -101,15 +96,20 @@ public class SlidingWindow
         return charCounts;
     }
 
-    private static bool CheckIfAllCharsExist(Dictionary<char, int> cmpFreq, Dictionary<char, int> testFreq)
+    public static int UniqueCharCount(ReadOnlySpan<char> input)
     {
-        foreach (var kvp in cmpFreq)
+        Span<bool> seen = stackalloc bool[52];
+        int uniqueCount = 0;
+
+        foreach (char c in input)
         {
-            if (!testFreq.TryGetValue(kvp.Key, out int count) || count < kvp.Value)
+            int index = (c >= 'a') ? (c - 'a' + 26) : (c - 'A');
+            if (!seen[index])
             {
-                return false;
+                seen[index] = true;
+                uniqueCount++;
             }
         }
-        return true;
+        return uniqueCount;
     }
 }
