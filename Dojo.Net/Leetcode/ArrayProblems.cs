0084f22fc68d9732e0a7cb79550d9bb502cb2ee2
Manoj Baishya <manojbaishya@users.noreply.github.com>
2025-04-01 23:36:04 +0530

    Add Quicksort()

--- Dojo.Net/Leetcode/ArrayProblems.cs
+++ Dojo.Net/Leetcode/ArrayProblems.cs
@@ -39,10 +39,10 @@ public class ArrayProblems
         int mid = (left + right) / 2;
         MergeSort(arr, left, mid);
         MergeSort(arr, mid + 1, right);
-        MergeSegments(arr, left, mid, right);
+        MergeSubarrays(arr, left, mid, right);
     }
 
-    private static void MergeSegments(int[] arr, int left, int mid, int right)
+    private static void MergeSubarrays(int[] arr, int left, int mid, int right)
     {
         int l = left;
         int r = mid + 1;
@@ -79,5 +79,53 @@ public class ArrayProblems
         {
             arr[i] = tmp[i - left];   
         }
+    }
+
+
+    public void QuickSort(int[] arr, int left, int right)
+    {
+        if (left < right)
+        {
+            int pivot = PartitionByPivot(arr, left, right);
+
+            QuickSort(arr, left, pivot - 1);
+            QuickSort(arr, pivot + 1, right);
+        }
+    }
+
+    private static int PartitionByPivot(int[] arr, int left, int right)
+    {
+        int pivot = MedianOfThree(arr, left, right);
+
+        (arr[pivot], arr[left]) = (arr[left], arr[pivot]); 
+        pivot = left;
+
+        (int i, int j) = (left + 1, right);
+
+        while(i < j)
+        {
+            while(arr[i] <= arr[pivot] && i < j) i++;
+            while(arr[j] > arr[pivot] && j > i) j--;
+
+            if (i < j) (arr[j], arr[i]) = (arr[i], arr[j]);
+        }
+
+        (arr[pivot], arr[i - 1]) = (arr[i - 1], arr[pivot]); 
+        pivot = i - 1;
+
+        return pivot;
+        
+    }
+
+    private static int MedianOfThree(int[] arr, int left, int right)
+    {
+        if(right - left == 1) return arr[left] <= arr[right] ? left : right;
+        
+        int mid = left + (right - left) / 2;
+        (int a, int b, int c) = (arr[left], arr[mid], arr[right]);
+        
+        if ((a <= b && b <= c) || (a >= b && b >= c)) return mid;
+        if ((a <= c && c <= b) || (a >= c && c >= b)) return right;
+        return left;
     }
 }
