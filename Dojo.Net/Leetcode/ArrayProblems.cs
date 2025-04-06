da8f3b37fbf6a859f3954e240e9fb4e03bcf1489
Manoj Baishya <manojbaishya@users.noreply.github.com>
2025-04-06 09:44:11 +0530

    Add SetMatrixZeroes() method

--- Dojo.Net/Leetcode/ArrayProblems.cs
+++ Dojo.Net/Leetcode/ArrayProblems.cs
@@ -598,4 +598,56 @@ public class ArrayProblems
         return max;
     }
 
+       public void SetZeroes(int[][] matrix) 
+    {
+        if (matrix.Length == 0) return; 
+        int M = matrix.Length;
+        int N = matrix[0].Length;
+
+        int col0 = 1;
+        for (int i = 0; i < M; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                if (matrix[i][j] == 0)
+                {
+                    matrix[i][0] = 0;
+                    if ( j != 0)
+                        matrix[0][j] = 0;
+                    else
+                        col0 = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < M; i++)
+        {
+            for (int j = 1; j < N; j++)
+            {
+                if (matrix[i][j] != 0)
+                {
+                    if (matrix[0][j] == 0 || matrix[i][0] == 0)
+                    {
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+        }
+
+        if (matrix[0][0] == 0)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                matrix[0][j] = 0;
+            }
+        }
+
+        if (col0 == 0)
+        {
+            for (int i = 0; i < M; i++)
+            {
+                matrix[i][0] = 0;
+            }
+        }
+    }
 }
